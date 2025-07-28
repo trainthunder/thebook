9cf414ea3a719a106adb1388a0150bd9899146a7
Kitikun <kitikun.po@gmail.com>
2025-07-28 21:00:13 +0700

    feat: install rspec and create unit test for book

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,6 @@ class Book < ApplicationRecord
 
   has_many :book_categories, dependent: :destroy
   has_many :categories, through: :book_categories
+
+  validates :name, presence: true
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "can be add new book with name, description, author and publisher" do
+    author = Author.create(name: "J.K. Rowling")
+    publisher = Publisher.create(name: "Bloomsbury")
+
+    book = Book.new(name: "Harry Potter", description: "Wizarding world", author: author, publisher: publisher)
+
+    expect(book).to be_valid
+  end
+
+  it "can't be add new book without name" do
+    author = Author.create(name: "J.K. Rowling")
+    publisher = Publisher.create(name: "Bloomsbury")
+
+    book = Book.new(name: nil, description: "Wizarding world", author: author, publisher: publisher)
+
+    expect(book).to be_invalid
+  end
+
+  it "can be add new book without description" do
+    author = Author.create(name: "J.K. Rowling")
+    publisher = Publisher.create(name: "Bloomsbury")
+
+    book = Book.new(name: "Harry Potter", description: nil, author: author, publisher: publisher)
+
+    expect(book).to be_valid
+  end
 end
